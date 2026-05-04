b0e76ec7e9214578c44836e0adfc755daaf088bb
erxes <dev@erxes.io>
2026-05-04 19:53:50 +0800

    Fix environment variables and GraphQL configuration

--- src/lib/apollo/server-client.ts
+++ src/lib/apollo/server-client.ts
@@ -8,8 +8,8 @@ export async function getServerApolloClient() {
   return new ApolloClient({
     link: new HttpLink({
       uri:
-        process.env.GRAPHQL_URL ??
-        process.env.NEXT_PUBLIC_GRAPHQL_URL ??
+        process.env.ERXES_ENDPOINT ??
+        process.env.NEXT_PUBLIC_ERXES_ENDPOINT ??
         "/graphql",
       headers: {
         "x-app-token": process.env.ERXES_APP_TOKEN ?? "",
